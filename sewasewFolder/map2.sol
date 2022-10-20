5835446c229d53d1bf0a65013a01edd8e9c4f2db
Remix Workspace <>
2022-10-20 16:03:23 +0300

    thursday

--- sewasewFolder/map2.sol
+++ sewasewFolder/map2.sol
@@ -15,8 +15,26 @@ contract tech{
     //function to insert address and amount
     function set(address _x, uint amount) public{
         balances[_x] = amount;
+        if(!inserted[_x]){
+            inserted[_x] = true;
+            keys.push(_x);
+        }
 
 
+    }
+    function getsize()public view returns(uint){
+        return keys.length;
+    }
+
+    function firstindex() public view returns(uint){
+        return balances[keys[0]];
+
+    }
+    function last() public view returns(uint){
+        return balances[keys[keys.length - 1]];
+    }
+    function anyindex(uint _x) public view returns(uint){
+        return balances[keys[_x]];
     }
 
 }
